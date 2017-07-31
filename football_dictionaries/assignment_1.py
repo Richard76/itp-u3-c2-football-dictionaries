7fc13cc375080354a65e5768aea94812ee71c383
Richard Farr <richard@paragonppc.com>
2017-07-31 21:46:12 +0000

    assignment 1 may work

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,17 @@
+
 def players_as_dictionaries(squads_list):
-    return ""
+    player_dict_list = []
+    for i in squads_list:
+        player_dict = {
+            'number': i[0],
+            'position': i[1],
+            'name':  i[2],
+            'date_of_birth': i[3],
+            'caps': i[4],
+            'club': i[5],
+            'country': i[6],
+            'club_country': i[7],
+            'year': i[8]
+        }
+        player_dict_list.append(player_dict)
+    return player_dict_list
